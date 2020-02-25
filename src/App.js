6908ddcc51087ff0ade3503ac58f3bd5c1c7d634
ehddnr301 <dy950328@gmail.com>
2020-02-25 19:39:35 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useReducer, useState } from "react";
+import uuid from "uuid/v4";
 
 const initialState = {
   toDos: []
 };
 
 const ADD = "add";
+const DEL = "del";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case ADD:
-      return { toDos: [...state.toDos, { text: action.payload }] };
+      return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
+    case DEL:
+      return { toDos: state.toDos.filter(toDo => toDo.id !== action.payload) };
     default:
       return;
   }
@@ -21,6 +25,7 @@ const App = () => {
   const onSubmit = e => {
     e.preventDefault();
     dispatch({ type: ADD, payload: newTodo });
+    setNewToDo("");
   };
   const onChange = e => {
     const {
@@ -42,8 +47,13 @@ const App = () => {
       </form>
       <ul>
         <h3>To Dos</h3>
-        {state.toDos.map((toDo, index) => (
-          <li key={index}>{toDo.text}</li>
+        {state.toDos.map(toDo => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              ❌
+            </button>
+          </li>
         ))}
       </ul>
     </>
