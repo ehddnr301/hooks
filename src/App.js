e3476139833cee11db15663ada0ac6df05ee0b3f
ehddnr301 <dy950328@gmail.com>
2020-02-25 19:09:41 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react";
-import Screen from "./Screen";
-import Lang from "./Context";
-import translations from "./translations";
+import React, { useReducer } from "react";
 
-function App() {
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+const reducer = (state, action) => {
+  switch (action) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
   return (
-    <Lang defaultLang="en" translations={translations}>
-      <Screen></Screen>
-    </Lang>
+    <>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch(INCREMENT)}>add</button>
+      <button onClick={() => dispatch(DECREMENT)}>remove</button>
+    </>
   );
-}
+};
 
 export default App;
--- src/translations.js
+++
@@ -1,9 +0,0 @@
-const translations = {
-  kr: {
-    Hello: "안녕",
-    thankYou: "고마워",
-    Translate: "번역"
-  }
-};
-
-export default translations;
